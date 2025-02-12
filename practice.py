383a2ecef0ea01deb11589062c5735e96d41d45d
MrTimur <timurtabs3@gmail.com>
2025-02-12 20:32:44 +0300

    core code done omg

--- practice.py
+++ practice.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import filedialog
 from time import sleep
 import threading
+import torch
+import numpy as np
+from PIL import Image
+from basicsr.archs.rrdbnet_arch import RRDBNet # model itself
+from realesrgan import RealESRGANer # framework that used it
 
 
 omg_slovar = {'input_file' : None, 'output_file' : None}
@@ -37,18 +42,51 @@ def choose_output_file():
     )
     print(omg_slovar['output_file'])
 
-def loading():
+def upscale_process():
     status_label.config(text='Loading fr')
-    sleep(10)
+
+    model_path = 'RealESRGAN_x4plus.pth'
+
+    state_dict = torch.load(model_path, map_location=torch.device('cpu'))['params_ema']
+
+    model = RRDBNet(
+        num_in_ch=3, 
+        num_out_ch=3, 
+        num_feat=64, 
+        num_block=23, 
+        num_grow_ch=32, 
+        scale=4
+        )
+    model.load_state_dict(state_dict, strict=True)
+
+    upsampler = RealESRGANer(
+        scale=4,
+        model_path=model_path,
+        model=model,
+        tile=0,
+        pre_pad=0,
+        half=False
+    )
+
     
+    picture_path = omg_slovar['input_file']
+
+
+    input_img = Image.open(picture_path).convert('RGB')
+
+    img = np.array(input_img)
+
+    output, _ = upsampler.enhance(img, outscale=4)
+
+    output_img = Image.fromarray(output)
+    output_img.save(omg_slovar['output_file'])
+
     status_label.config(text='Done')
-    sleep(1)
-    status_label.config(text='Status')
 
 def start():
 
     
-    threading.Thread(target=loading).start()
+    threading.Thread(target=upscale_process).start()
 
 button_start = tk.Button(
     root,
